8500be62df4216a4cb0cbbef12ab92e99f1e2c85
data-engineer-dare <dare.david.olufunmilayo@gmail.com>
2017-02-24 12:15:26 +0000

    Refactored the code

--- StatsAlgorithms/stats.py
+++ StatsAlgorithms/stats.py
@@ -7,26 +7,23 @@ else:
 
         def __init__(self, data):
             self.data = data
-            self.upper = max(self.data)
-            self.lower = min(self.data)
-            self.classwidth = int(float(self.upper)/self.lower + (self.upper % self.lower > 0))     # Always round up the result
+            self.upper = max(self.data)                 #Get the lower limit of the data
+            self.lower = min(self.data)                 #Get the upper limit of the data
+            self.classwidth = int(float(self.upper)/self.lower + (self.upper % self.lower > 0))     # Compute the classwidth and Always round up the result
 
-    # Take care of capturing the last bit of the tuples
+#Define the function that distributes the data based on the classwidth
 
         def distributeClassWidth(self):
-            self.iter = 0   # Use this to check the iteration is running the first time or not.
-            self.count = 0  # Use this for the while loop
-            self.tempdata = []
-            self.data_distribution = range(self.lower, self.upper + 1, self.classwidth)
-            for self.n in range(self.upper):
-                if self.iter == 0:
-                    self.tup1 = self.lower
+            self.iter = 0                                 # Use the iter variable to coordinate what to code run the first time and subsequent times
+            self.tempdata = []                            #Get an empty List
+            for self.n in range(self.upper):              #Start the loop here
+                if self.iter == 0:                        #Check if this is the first loop
+                    self.tup1 = self.lower                #Even though i am not using TUPLES, i am using the terminology to indicate that the sub-lists are in the form of a tuple
                     self.tup2 = self.tup1 + self.classwidth
                     self.tuple = [self.tup1, self.tup2]
                     self.tempdata.append(self.tuple)
                     self.tup1 = self.tup2 + 1
                     self.iter += 1
-
                 else:
                     self.tup2 = self.tup1 + self.classwidth
                     self.tuple = [self.tup1, self.tup2]
@@ -35,10 +32,6 @@ else:
                         break
                     self.tup1 = self.tup2 + 1
             return self.tempdata
-
-        '''def classMidPoint(self):
-            for self.tuple in self.tempdata:
-                print (self.tuple)'''
 
         def __str__(self):
             return self.tempdata
